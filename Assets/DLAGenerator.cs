8084d69deb7698b1995098357700bf08f7719fdd
Hansonbg <101675228+Hansonbg@users.noreply.github.com>
2025-04-28 20:03:15 -0400

    Added logic to the upscale copy to ignore copying points that were surrounded on top and left sides in the old map. Added pseudorandom wiggle to right and down connections.

--- Assets/DLAGenerator.cs
+++ Assets/DLAGenerator.cs
@@ -162,19 +162,26 @@ bool[,] UpscaleDLA(bool[,] oldMap, int newSize)
             if (!oldMap[x, y])
                 continue;
 
+            bool upLeftExists = (x - 1 >= 0 && y - 1 >= 0 && oldMap[x - 1, y - 1]);
+            bool leftExists = (x - 1 >= 0) && (oldMap[x - 1, y]);
+            bool upExists = (y - 1 >= 0) && (oldMap[x, y - 1]); 
+            
             int hx = 2 * x + 1;
             int hy = 2 * y + 1;
-            if (!upscaledMap[hx, hy])
-            {
-                upscaledMap[hx, hy] = true;
-                filledCount++;
-            }
 
+            // Skip copying points that were surrounded on the top and left in the low-res
+            // map and decrement the count there
+            if (upLeftExists && leftExists && upExists)
+                filledCount--;
+            else
+                upscaledMap[hx, hy] = true; 
         }
         
         // Second pass checks whether a point in the old map had a connection to the
         // right and the bottom, and if so, adds a corresponding point in the upscaled
         // map
+        int randomSwitch;
+        
         for (int y = 0; y < oldSize; ++y)
         for (int x = 0; x < oldSize; ++x)
         {
@@ -183,16 +190,54 @@ bool[,] UpscaleDLA(bool[,] oldMap, int newSize)
             
             int hx = 2 * x + 1;
             int hy = 2 * y + 1;
-            
-            if (x + 1 < oldSize && oldMap[x + 1, y]) {
-                upscaledMap[hx + 1, hy] = true;     // E
-                filledCount++;
+           
+            bool downExists = (y + 1 < oldSize && oldMap[x, y + 1]);
+            bool rightExists = (x + 1 < oldSize && oldMap[x + 1, y]);
+           
+            // If the right pixel existed, we will favor adding a right connection
+            // with a chance of it turning into an up-right or down-right connection instead.
+            if (rightExists) {
+                randomSwitch = Random.Range(0, 8);
+                if (randomSwitch >= 0 && randomSwitch < 4)
+                {
+                    // extend the right connection
+                    upscaledMap[hx + 1, hy] = true; // E
+                    filledCount++;
+                } else if (randomSwitch >= 4 && randomSwitch < 6 && hy - 1 >= 0)
+                {
+                    // add an up-right connection
+                    upscaledMap[hx + 1, hy - 1] = true;
+                    filledCount++;
+                }
+                else if (hx + 1 < newSize && hy + 1 < newSize)
+                {
+                    // add a down-right connection
+                    upscaledMap[hx + 1, hy + 1] = true;
+                    filledCount++;
+                }
             }
 
-            if (y + 1 < oldSize && oldMap[x, y + 1])
+            if (downExists)
             {
-                upscaledMap[hx, hy + 1] = true; // S
-                filledCount++;
+                randomSwitch = Random.Range(0, 8);
+
+                if (randomSwitch >= 0 && randomSwitch < 4 && hy + 1 < newSize)
+                {
+                    // extend the down connection
+                    upscaledMap[hx, hy + 1] = true; // S
+                    filledCount++;
+                } else if (randomSwitch >= 4 && randomSwitch < 6 && hx - 1 >= 0 && hy + 1 < newSize)
+                {
+                    // instead wiggle down left
+                    upscaledMap[hx - 1, hy + 1] = true;
+                    filledCount++;
+                }
+                else if (hx + 1 < newSize && hy + 1 < newSize)
+                {
+                    // instead wiggle down right
+                    upscaledMap[hx + 1, hy + 1] = true;
+                    filledCount++;
+                }
             }
         }
         
@@ -222,7 +267,7 @@ bool[,] UpscaleDLA(bool[,] oldMap, int newSize)
                 if (upRightExists && downRightExists)
                 {
                     
-                    if (Random.Range(0,2) == 0)
+                    if (Random.Range(0,200) % 2 == 0)
                     {
                         upscaledMap[hx + 1, hy + 1] = true;
                         filledCount++;
